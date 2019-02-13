c097963ee02965074d9288ef3fdd2387a8f5b6f9
Laspargus <maxime.speroni@gmail.com>
2019-02-13 16:35:14 +0100

    preparation for heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,66 +9,31 @@
 
 require 'faker'
 
-#User.destroy_all
-#Event.destroy_all
-#Attendance.destroy_all
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
 
 
 
 # - Je crée mes Utilisateurs
-5.times do |index|
-	u = User.create(email: Faker::Internet.email , encrypted_password: SecureRandom.urlsafe_base64 ,description:Faker::Dune.saying ,first_name: Faker::DragonBall.character , last_name: Faker::Zelda.character)
+10.times do |index|
+	u = User.create(email: Faker::Internet.email , password: 'Bonjour' ,description:Faker::Dune.saying ,first_name: Faker::DragonBall.character , last_name: Faker::Zelda.character)
 	
 	p "L'utilisateur #{u.first_name} a été crée"
 end
 
 
 #Je crée mes évènements
-10.times do |index|
-	e=Event.create(start_date: DateTime.new(2019,rand(1..12),rand(1..28)), duration:[30,45,60,90].sample , title:Faker::HarryPotter.house , description:Faker::HarryPotter.quote , price: rand(1..1000) , location:Faker::WorldCup.city , admin: User.all.sample)
-	p "L'evenement #{e.title} a été crée"
-end
-
-
-#Je crée mes attendances
-100.times do |index|
-a=Attendance.create(stripe_customer_id:  SecureRandom.urlsafe_base64, user: User.all.sample, event: Event.all.sample)
-p "Une nouvelle Attendance avec  #{a.user.first_name} partcipant à #{a.event.title} a été crée" 
-end
-
-=begin
-
-#for test mail
-1.times do |index|
-	u = User.create(email: "micheleventbrite@yopmail.com" , encrypted_password: SecureRandom.urlsafe_base64 ,description:Faker::Dune.saying ,first_name: Faker::DragonBall.character , last_name: Faker::Zelda.character)	
-	p "L'utilisateur #{u.first_name} a été crée"
-end
-
-
-
-
-
-# - Je crée mes Utilisateurs
-1.times do |index|
-	u = User.create(email: "micheleventbrite@yopmail.com" , encrypted_password: SecureRandom.urlsafe_base64 ,description:Faker::Dune.saying ,first_name: Faker::DragonBall.character , last_name: Faker::Zelda.character)
-	
-	p "L'utilisateur #{u.first_name} a été crée"
-end
-
-
-
-#Je crée mes évènements
 5.times do |index|
-	e=Event.create(start_date: DateTime.new(2019,rand(1..12),rand(1..28)), duration:[30,45,60,90].sample , title:Faker::HarryPotter.house , description:Faker::HarryPotter.quote , price: rand(1..1000) , location:Faker::WorldCup.city , admin: User.all.sample)
+	e=Event.create(start_date: DateTime.new(2019,rand(3..12),rand(1..28)), duration:[30,45,60,90].sample , title:Faker::HarryPotter.house , description:Faker::HarryPotter.quote , price: rand(1..1000) , location:Faker::WorldCup.city , admin: User.all.sample)
 	p "L'evenement #{e.title} a été crée"
 end
 
 
 #Je crée mes attendances
-10.times do |index|
+50.times do |index|
 a=Attendance.create(stripe_customer_id:  SecureRandom.urlsafe_base64, user: User.all.sample, event: Event.all.sample)
 p "Une nouvelle Attendance avec  #{a.user.first_name} partcipant à #{a.event.title} a été crée" 
 end
 
-=end
 
