d2b29256c7838d6593df74606c31593d00885261
Laspargus <maxime.speroni@gmail.com>
2019-02-13 18:50:29 +0100

    heroku mail debug ?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+#heroku
+  config.action_mailer.default_url_options = { :host => ‘eventbrite-max.herokuapp.com’ }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     last_name { "Doe" }
     email { "maxime.speroni@yopmail.com" }    
     description {"Organisateur de parties fines"}
-    encrypted_password {SecureRandom.urlsafe_base64}
+    password {'Bonjour'}
   end
 end
 
