eead40bfbecb7aeba567babac6e4ad9c0124409c
Laspargus <maxime.speroni@gmail.com>
2019-02-13 19:11:21 +0100

    suppress mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 #heroku
-  config.action_mailer.default_url_options = { :host => ‘eventbritemax.herokuapp.com’ }
+#  config.action_mailer.default_url_options = { :host => ‘eventbritemax.herokuapp.com’ }
 end
